dca9bbae6fa77e9c4b98a2c175016a902fc3b2d7
Peterson Germain <e2124621@cmaisonneuve.qc.ca>
2025-10-24 16:26:48 -0400

    Probleme avec le has du network object

--- Battle Ship/Battle Ship/Assets/Scripts/GameManager.cs
+++ Battle Ship/Battle Ship/Assets/Scripts/GameManager.cs
@@ -3,23 +3,31 @@ using Unity.Netcode;
 using UnityEngine.SceneManagement;
 using System;
 using Unity.Services.Matchmaker.Models;
+using Unity.Netcode.Transports.UTP;
+using System.Collections;
 
 public class GameManager : NetworkBehaviour
 {
 
-  public static GameManager instance;
-  public GameObject panelDeConnection;
-  public GameObject panelAttente;
+    public static GameManager singleton { get; private set; }
+    public GameObject panelDeConnection;
+    public GameObject panelAttente;
 
-  void Awake() {
-    if (instance == null) {
-      instance = this;
-    }
+     public GameObject bateau;
 
-    else {
-      Destroy(gameObject);
+  private void Awake()
+    {
+        if (singleton == null)
+        {
+            singleton = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
     }
-  }
 
   public override void OnNetworkSpawn()
     {
@@ -80,7 +88,37 @@ public class GameManager : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!IsOwner) return; // each client only controls their own clicks
 
+        // Detect touch or mouse click
+        if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) ||
+            Input.GetMouseButtonDown(0))
+        {
+            Vector2 screenPos = Input.touchCount > 0
+                ? Input.GetTouch(0).position
+                : (Vector2)Input.mousePosition;
+
+            // Create a ray from the camera through the click position
+            Ray ray = Camera.main.ScreenPointToRay(screenPos);
+
+            // We'll hit a flat plane (like ground or ocean)
+            Plane groundPlane = new Plane(Vector3.up, Vector3.zero); // y = 0 plane
+            float distance;
+
+            if (groundPlane.Raycast(ray, out distance))
+            {
+                Vector3 worldPos = ray.GetPoint(distance);
+                // Ask the server to spawn the boat there
+                SpawnBoatServerRpc(worldPos);
+            }
+        }
+    }
+    
+    [ServerRpc]
+    void SpawnBoatServerRpc(Vector3 position, ServerRpcParams rpcParams = default)
+    {
+        GameObject bateauCopie = Instantiate(bateau, position, Quaternion.identity);
+        bateauCopie.GetComponent<NetworkObject>().SpawnWithOwnership(OwnerClientId);
     }
 }
 
--- Battle Ship/Battle Ship/Assets/Scripts/Joueur.cs
+++ Battle Ship/Battle Ship/Assets/Scripts/Joueur.cs
@@ -3,46 +3,40 @@ using Unity.Netcode;
 
 public class Joueur : NetworkBehaviour
 {
+    public GameObject bateau; // assign in inspector
 
-    public GameObject bateau;
-
-    public override void OnNetworkSpawn() 
-    {
-        base.OnNetworkSpawn();
-
-        if(IsServer) 
-        {
-            if(Input.GetMouseButtonDown(0)) 
-            {
-                Vector3 touchPosition = Input.mousePosition;
-
-                Vector3 worldPosition = Camera.main.ScreenToWorldPoint(touchPosition);
-
-                Instantiate(bateau, worldPosition, Quaternion.identity);
-            }
-        }
-
-        else 
-        {
-            if(Input.GetMouseButtonDown(0)) 
-            {
-                Vector3 touchPosition = Input.mousePosition;
-
-                Vector3 worldPosition = Camera.main.ScreenToWorldPoint(touchPosition);
-
-                Instantiate(bateau, worldPosition, Quaternion.identity);
-            }
-        }
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+    //     if (!IsOwner) return; // each client only controls their own clicks
+
+    //     // Detect touch or mouse click
+    //     if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) ||
+    //         Input.GetMouseButtonDown(0))
+    //     {
+    //         Vector2 screenPos = Input.touchCount > 0 
+    //             ? Input.GetTouch(0).position 
+    //             : (Vector2)Input.mousePosition;
+
+    //         // Create a ray from the camera through the click position
+    //         Ray ray = Camera.main.ScreenPointToRay(screenPos);
+
+    //         // We'll hit a flat plane (like ground or ocean)
+    //         Plane groundPlane = new Plane(Vector3.up, Vector3.zero); // y = 0 plane
+    //         float distance;
+
+    //         if (groundPlane.Raycast(ray, out distance))
+    //         {
+    //             Vector3 worldPos = ray.GetPoint(distance);
+    //             // Ask the server to spawn the boat there
+    //             SpawnBoatServerRpc(worldPos);
+    //         }
+    //     }
     }
+
+    // [ServerRpc]
+    // void SpawnBoatServerRpc(Vector3 position, ServerRpcParams rpcParams = default)
+    // {
+    //     GameObject bateauCopie = Instantiate(bateau, position, Quaternion.identity);
+    //     bateauCopie.GetComponent<NetworkObject>().SpawnWithOwnership(OwnerClientId);
+    // }
 }
